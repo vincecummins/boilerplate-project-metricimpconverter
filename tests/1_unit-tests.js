044de05db30d059805f108817e7c53719962da25
Vincent Cummins <vincent.cummins8@gmail.com>
2021-04-22 21:28:20 +1000

    first few unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,41 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite('Unit Tests', function () {
+
+    suite('Function convertHandler.getNum(input)', function () {
+
+        test('Whole number input', function (done) {
+            var input = '32mi';
+            assert.equal(convertHandler.getNum(input), 32);
+            done();
+        });
+
+        test('decimal number input', function (done) {
+            var input = '32.6mi';
+            assert.equal(convertHandler.getNum(input), 32.6);
+            done();
+        });
+
+        test('fractional input', function (done) {
+            var input = '1/2mi';
+            assert.equal(convertHandler.getNum(input), 1/2);
+            done();
+        });
+
+        test('fractional input with decimal', function (done) {
+            var input = '1/2.5mi';
+            assert.equal(convertHandler.getNum(input), 1/2.5);
+            done();
+        });
+
+        test('double fraction', function (done) {
+            var input = '1/2/1mi';
+            assert.equal(convertHandler.getUnit(input), false);
+            done();
+        });
+
+
+    })
 
 });
